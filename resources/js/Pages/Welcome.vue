41a8066393e6a13962f87f363dec229cfc7645a4
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-08 18:43:10 +0200

    Add Dark Mode Toggle Button

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import { ref } from 'vue'
 
 defineProps({
     canLogin: {
@@ -25,9 +26,10 @@ function handleImageError() {
     document.getElementById('background')?.classList.add('!hidden');
 }
 
+const isDarkMode = ref(false);
 const switchMode = function () {
-    let app = document.getElementById('app');
-    app.classList.contains('dark') === true ? app.classList.remove('dark') : app.classList.add('dark');
+  let app = document.getElementById('app');
+  app.classList.contains('dark') === true ? app.classList.remove('dark') : app.classList.add('dark');
 }
 
 
@@ -87,18 +89,36 @@ const switchMode = function () {
                     </nav>
 
                     <nav class="-mx-3 flex flex-1 justify-end">
-                        <button  aria-label="Theme Switcher"
-                                 @click="switchMode"
+
+                        <button
+                            aria-label="Theme Switcher"
+                            @click="isDarkMode = !isDarkMode; switchMode()"
                            class="ml-8 bg-primary-light dark:bg-ternary-dark px-3 py-2 shadow-sm rounded-xl cursor-pointer"
                            data-v-667c54d4="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            <svg v-if="isDarkMode"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-sun text-gray-200 hover:text-gray-50 w-5">
+                                <circle cx="12" cy="12" r="5"></circle>
+                                <line x1="12" y1="1" x2="12" y2="3"></line>
+                                <line x1="12" y1="21" x2="12" y2="23"></line>
+                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                                <line x1="1" y1="12" x2="3" y2="12"></line>
+                                <line x1="21" y1="12" x2="23" y2="12"></line>
+                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                            </svg>
+                            <svg v-else
+                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                  stroke-linejoin="round"
                                  class="feather feather-moon text-liText-ternary-dark hover:text-gray-400 dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light w-5">
                                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
                             </svg>
-                        </button>
 
+
+                        </button>
                     </nav>
 
                 </header>
