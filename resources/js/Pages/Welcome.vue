8a84108197b29c9d61d059a47ac7297abd52acdd
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-08 17:13:18 +0200

    Dark Mode Switcher Added

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -24,6 +24,13 @@ function handleImageError() {
     document.getElementById('docs-card-content')?.classList.add('!flex-row');
     document.getElementById('background')?.classList.add('!hidden');
 }
+
+const switchMode = function () {
+    let app = document.getElementById('app');
+    app.classList.contains('dark') === true ? app.classList.remove('dark') : app.classList.add('dark');
+}
+
+
 </script>
 
 <template>
@@ -78,6 +85,22 @@ function handleImageError() {
                             </Link>
                         </template>
                     </nav>
+
+                    <nav class="-mx-3 flex flex-1 justify-end">
+                        <button  aria-label="Theme Switcher"
+                                 @click="switchMode"
+                           class="ml-8 bg-primary-light dark:bg-ternary-dark px-3 py-2 shadow-sm rounded-xl cursor-pointer"
+                           data-v-667c54d4="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 class="feather feather-moon text-liText-ternary-dark hover:text-gray-400 dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light w-5">
+                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                            </svg>
+                        </button>
+
+                    </nav>
+
                 </header>
 
                 <main class="mt-6">
