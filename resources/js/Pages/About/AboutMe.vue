6099d7329e3dbe13cab0f492ea0c3c07bf3e62a9
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-14 17:07:15 +0200

    img url changed

--- resources/js/Pages/About/AboutMe.vue
+++ resources/js/Pages/About/AboutMe.vue
@@ -24,7 +24,7 @@ export default {
         <!-- About profile image -->
         <div class="w-full sm:w-1/4 mb-7 sm:mb-0">
             <img
-                src="@/assets/images/profile.jpeg"
+                src="/images/about/me.jpg"
                 class="rounded-xl w-96"
                 alt=""
             />
