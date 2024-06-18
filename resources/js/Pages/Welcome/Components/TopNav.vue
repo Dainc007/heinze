163d0c734cb4a62f7c43b6bd92aa027ecffe1264
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-18 18:13:45 +0200

    Fixing Dark Mode broke after redirect

--- resources/js/Pages/Welcome/Components/TopNav.vue
+++ resources/js/Pages/Welcome/Components/TopNav.vue
@@ -4,19 +4,8 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import DarkModeButton from "@/Components/DarkModeButton.vue";
 
-function isDarkMode() {
-    return localStorage.getItem("mode") === 'dark';
-}
-
-if(isDarkMode() === true) {
-    switchMode();
-}
-function switchMode() {
-    let app = document.getElementById('app');
-    app.classList.contains('dark') === true ? app.classList.remove('dark') : app.classList.add('dark');
-    app.classList.contains('dark') === true ? localStorage.setItem("mode", "dark") : localStorage.setItem("mode", 'light');
-}
 const showingNavigationDropdown = ref(false);
 </script>
 <style>
@@ -67,36 +56,7 @@ const showingNavigationDropdown = ref(false);
                         </NavLink>
                     </div>
                 </div>
-
-                <div class="shrink-0 flex items-center">
-                    <button
-                        aria-label="Theme Switcher"
-                        @click="switchMode()"
-                        class="ml-8 bg-primary-light dark:bg-ternary-dark px-3 py-2 shadow-sm rounded-xl cursor-pointer"
-                        data-v-667c54d4="">
-                        <svg id="sun"
-                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="hidden dark:block feather feather-sun text-liText-ternary-dark hover:text-amber-50 dark:text-amber-300 dark:hover:border-2 dark:hover:rounded-full dark:hover:border-amber-100 w-10">
-                            <circle cx="12" cy="12" r="5"></circle>
-                            <line x1="12" y1="1" x2="12" y2="3"></line>
-                            <line x1="12" y1="21" x2="12" y2="23"></line>
-                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                            <line x1="1" y1="12" x2="3" y2="12"></line>
-                            <line x1="21" y1="12" x2="23" y2="12"></line>
-                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                        </svg>
-                        <svg
-                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round"
-                             class="dark:hidden feather feather-moon text-liText-ternary-dark hover:text-amber-300 dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light w-10">
-                            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                        </svg>
-                    </button>
-                </div>
+                <DarkModeButton></DarkModeButton>
                 <!-- Hamburger -->
                 <div class="-me-2 flex items-center sm:hidden">
                     <button
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,13 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+        <script>
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
