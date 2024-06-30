6e9d4f49000800c59cd8c7bfaf2c36c881616457
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-30 11:39:39 +0200

    add meta description and keywords

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Programowanie, Tworzenie Stron Internetowych Firma: Daniel Heinze, Portfolio i CV NIP 9910548604">
+        <meta name="keywords" content="Daniel Heinze Programowanie Strony Internetowe Laravel Filament Livewire Vue Sklepy e-commerce WHMCS Portfolio Projekty Firma">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Heinze Daniel Heinze') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
